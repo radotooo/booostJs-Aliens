fdb82f74a35cc973212b4457d6909aa1c02cd794
radotooo <radotooo@gmail.com>
2021-01-28 20:46:22 +0200

    fix: wrong name

--- src/app/custom/Saucer.js
+++ src/app/custom/Saucer.js
@@ -16,16 +16,16 @@ export default class Saucer extends EventEmitter {
   async moveTo(condition) {
     const x = condition === 'in' ? -835 : -1800;
     const id = condition === 'in' ? 'flyIn' : 'flyOut';
-    const event =
+    const eventData =
       condition === 'in' ? Saucer.events.FLY_IN : Saucer.events.FLY_AWAY;
 
     await gsap.to(this._saucerElement, {
       x,
       id,
       ease: 'power3.in',
-      duration: 3,
+      duration: 2,
     });
-    this.emit(event);
+    this.emit(eventData);
   }
 
   async toggleBeam(condition) {
@@ -33,14 +33,13 @@ export default class Saucer extends EventEmitter {
     const opacity = condition === 'in' ? 0.6 : 0;
     const idTop = condition === 'in' ? 'showTopBeam' : 'hideTopBeam';
     const idBot = condition === 'in' ? 'showBottomBeam' : 'hideBottomBeam';
-
-    const event =
+    const eventData =
       condition === 'in' ? Saucer.events.BAM_SHOW : Saucer.events.BEAM_HIDE;
 
     await animation
       .to(this._beamTopElement, { duration: 1, id: idTop, opacity }, '')
       .to(this._beamBottomElement, { duration: 1, id: idBot, opacity }, '');
-    this.emit(event);
+    this.emit(eventData);
   }
 
   static get events() {
